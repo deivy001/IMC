a0006d711102c8e546e04cf5dbd82916a825619b
deivy benites <1507829@senati.pe>
2025-04-15 12:23:17 -0500

    se actualizo el node

--- calcularIMC.js
+++ calcularIMC.js
@@ -2,10 +2,4 @@ function calcularIMC(peso, altura) {
     const imc = peso / (altura * altura);
     return imc;
 }
-const pesoUsuario= 80;
-const alturaUsuario= 1.55
-const imcUsuario= calcularIMC(pesoUsuario, alturaUsuario);
-
-//imcUsuario.toFixed(2);
-
-console.log("el valor del IMC es:"+ imcUsuario.toFixed(2));
+Module.exports.moduloCalarIMC
